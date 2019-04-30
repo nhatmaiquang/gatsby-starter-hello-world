de8636e16bffa9b1734aa2ccf2a85a50d30fa039
Mai Quang Nhat <quangnhat224@gmail.com>
2019-04-30 14:36:15 +0300

    some graphql stuff

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+/*
 import React from "react"
 import Header from "../components/header"
 
@@ -7,4 +8,39 @@ export default () => (
     <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
   </div>
+)*/
+
+/*
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <div>
+    <h1>About me</h1>
+    <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
+  </div>
+)*/
+
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>
+      We're the only site running on your computer dedicated to showing the best
+      photos and videos of pandas eating lots of food.
+    </p>
+  </Layout>
 )
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+/*
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
@@ -9,4 +10,33 @@ export default () => (
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
+)*/
+
+/*
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </Layout>
+)*/
+
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+  <Layout>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+  </Layout>
 )
